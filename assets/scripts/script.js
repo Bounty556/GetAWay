b04b9b007cdf49a945ed40e248ec93b66fc8b86d
Cade Clark <crazycademan@gmail.com>
2020-04-08 18:10:45 -0600

    Added CDNs as well as basic nav bar and linked Style and Script files

---
+++ assets/scripts/script.js
@@ -0,0 +1,4 @@
+var searchCity = //User Input Here;
+
+// Amadeus City/Airport Search Link
+var queryURL = ("https://test.api.amadeus.com/v1/reference-data/locations?subType=AIRPORT,CITY&keyword=" +  searchCity +"&page[limit]=5");
