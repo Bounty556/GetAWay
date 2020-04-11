e3565a919d955f707b3671f86ddd05d8ba3e3b5c
Bounty556 <jacobmayday@gmail.com>
2020-04-11 13:02:47 -0600

    All content from April 11th class

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,12 +14,35 @@ $(document).ready(function() {
   setSearchMethod('Title');
   
   $(document).on('keydown', '#search-bar', tempSearch);
+
+  // Update search method when clicked
   $('#dropdown1').on('click', 'a', function() {
     setSearchMethod($(this).text());
   });
   
   // Drop Down Functionality
-  $('.dropdown-trigger').dropdown();
+  $('#fav-list').dropdown();
+  $('#search-by').dropdown();
+
+  //TODO: Fix this tomorrow when I get my API key
+//   $.ajax({
+//     url: 'https://cors-anywhere.herokuapp.com/https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search',
+//     method: 'GET',
+//     headers: {
+//       q: 'testing'
+//     },
+//     data: {
+//       q: 'testing'
+//     },
+//     error: function (error) {
+//       console.log(error);
+//     }
+//   }).then(function(response) {
+//     console.log(response);
+//   });
+
+
+  makeAmazonAPICall('Beastars', 'Paru Itagaki');
 });
 
 // Runs a search on OpenLibrary's search engine by a book's title,
@@ -131,7 +154,53 @@ function tempSearch(e) {
 
 // Sets what method we're searching for a book with
 function setSearchMethod(searchOption) {
-  $('.dropdown-trigger').text(searchOption);
+  $('#search-by').text(searchOption);
 
   searchMethod = searchMethods[searchOption.toLowerCase()];
+}
+
+function makeAmazonAPICall(title, author) {
+  let accessKey = 'AKIAI2EYVAYC5BALQWYQ';
+  let secret = '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';
+  let timestamp = new Date().toISOString();
+
+  timestamp = timestamp.substr(0, timestamp.indexOf('.')) + '.000Z';
+
+  let params = [
+    'AWSAccessKeyId=' + accessKey,
+    'Author=' + encodeURIComponent(author),
+    'Operation=ItemSearch',
+    'SearchIndex=Books',
+    'Service=AWSECommerceService',
+    'Timestamp=' + encodeURIComponent(timestamp),
+    'Title=' + encodeURIComponent(title),
+    'Version=' + encodeURIComponent('2013-08-01')
+  ];
+
+  let queryString = '';
+
+  for (let i = 0; i < params.length; i++) {
+    queryString = queryString.concat(params[i] + '&');
+  }
+
+  let stringToSign = `GET
+  webservices.amazon.com
+  /onca/xml
+  `+ queryString;
+
+  let hash = CryptoJS.HmacSHA256(stringToSign, secret);
+  var hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
+  hashInBase64 = hashInBase64.replace(/[+]/g, '%2B');
+  hashInBase64 = hashInBase64.replace(/=/g, '%3D');
+
+  queryString = queryString.concat('Signature=' + hashInBase64);
+
+  console.log(queryString);
+
+  $.ajax({
+    url: 'https://cors-anywhere.herokuapp.com/https://webservices.amazon.com/onca/xml?' + queryString,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response);
+  });
 }
